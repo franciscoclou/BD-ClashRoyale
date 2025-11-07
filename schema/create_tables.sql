c299c99023c4f4163e1893f5c91ee1f27629a912
franciscoclou <francisco.c.lourenco@gmail.com>
2025-11-07 22:26:54 +0000

    Schemas first commit

---
+++ schema/create_tables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE
+    Player (
+        player_id INT PRIMARY KEY NOT NULL,
+        username VARCHAR(50) NOT NULL,
+        level_ INT NOT NULL CHECK (level >= 0),
+        trophies INT NOT NULL CHECK (trophies >= 0),
+        gold INT NOT NULL CHECK (gold >= 0),
+        gems INT NOT NULL CHECK (gems >= 0),
+        max_trhophies INT NOT NULL CHECK (max_trhophies >= 0),
+        battles_won INT NOT NULL CHECK (battles_won >= 0)
+    );
