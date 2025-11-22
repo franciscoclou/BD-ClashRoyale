d0e5064d5a37e5e2fed0d0465fd6daf1a9742d35
rodrigo-roque <rodrigohroque@gmail.com>
2025-11-22 13:39:20 +0000

    added all cards to the populate

--- queries/populate1.sql
+++ queries/populate1.sql
@@ -70,24 +70,264 @@ INSERT INTO Gem (item_id) VALUES (2);
 
 -- Items 3 a 6 são Cartas
 -- Nota: Inserimos primeiro na tabela Item, depois na tabela Card
-INSERT INTO Item (item_id, item_quantity) VALUES (3, 1); -- Knight
-INSERT INTO Item (item_id, item_quantity) VALUES (4, 1); -- Archers
-INSERT INTO Item (item_id, item_quantity) VALUES (5, 1); -- Giant
-INSERT INTO Item (item_id, item_quantity) VALUES (6, 1); -- Fireball
+INSERT INTO Item (item_id, item_quantity) VALUES
+INSERT INTO Item (item_id, item_quantity) VALUES
+-- 1 Elixir
+(3, 1), -- Skeletons
+(4, 1), -- Ice Spirit
+(5, 1), -- Fire Spirit
+(6, 1), -- Electro Spirit
+(7, 1), -- Heal Spirit
+(8, 1), -- Mirror (Variable Cost)
+
+-- 2 Elixir
+(9, 1), -- Goblins
+(10, 1), -- Spear Goblins
+(11, 1), -- Bats
+(12, 1), -- Bomber
+(13, 1), -- Ice Golem
+(14, 1), -- Zap
+(15, 1), -- Giant Snowball
+(16, 1), -- The Log
+(17, 1), -- Barbarian Barrel
+(18, 1), -- Rage
+(19, 1), -- Wall Breakers
+
+-- 3 Elixir
+(20, 1), -- Knight
+(21, 1), -- Archers
+(22, 1), -- Minions
+(23, 1), -- Goblin Gang
+(24, 1), -- Mega Minion
+(25, 1), -- Guards
+(26, 1), -- Dart Goblin
+(27, 1), -- Skeleton Army
+(28, 1), -- Tombstone
+(29, 1), -- Cannon
+(30, 1), -- Goblin Barrel
+(31, 1), -- Skeleton Barrel
+(32, 1), -- Firecracker
+(33, 1), -- Arrows
+(34, 1), -- Clone
+(35, 1), -- Tornado
+(36, 1), -- Earthquake
+(37, 1), -- Royal Delivery
+(38, 1), -- Void
+(39, 1), -- Princess
+(40, 1), -- Ice Wizard
+(41, 1), -- Miner
+(42, 1), -- Bandit
+(43, 1), -- Royal Ghost
+(44, 1), -- Fisherman
+(45, 1), -- Elixir Golem
+(46, 1), -- Little Prince (Champion)
+
+-- 4 Elixir
+(47, 1), -- Valkyrie
+(48, 1), -- Musketeer
+(49, 1), -- Mini P.E.K.K.A
+(50, 1), -- Hog Rider
+(51, 1), -- Battle Ram
+(52, 1), -- Flying Machine
+(53, 1), -- Zappies
+(54, 1), -- Furnace
+(55, 1), -- Bomb Tower
+(56, 1), -- Tesla
+(57, 1), -- Goblin Cage
+(58, 1), -- Goblin Drill
+(59, 1), -- Mortar
+(60, 1), -- Skeleton Dragons
+(61, 1), -- Battle Healer
+(62, 1), -- Fireball
+(63, 1), -- Poison
+(64, 1), -- Freeze
+(65, 1), -- Electro Wizard
+(66, 1), -- Inferno Dragon
+(67, 1), -- Night Witch
+(68, 1), -- Lumberjack
+(69, 1), -- Hunter
+(70, 1), -- Magic Archer
+(71, 1), -- Mother Witch
+(72, 1), -- Phoenix
+(73, 1), -- Golden Knight (Champion)
+(74, 1), -- Skeleton King (Champion)
+(75, 1), -- Mighty Miner (Champion)
+
+-- 5 Elixir
+(76, 1), -- Giant
+(77, 1), -- Wizard
+(78, 1), -- Witch
+(79, 1), -- Prince
+(80, 1), -- Baby Dragon
+(81, 1), -- Barbarians
+(82, 1), -- Minion Horde
+(83, 1), -- Rascals
+(84, 1), -- Royal Hogs
+(85, 1), -- Balloon
+(86, 1), -- Cannon Cart
+(87, 1), -- Inferno Tower
+(88, 1), -- Goblin Hut
+(89, 1), -- Executioner
+(90, 1), -- Bowler
+(91, 1), -- Graveyard
+(92, 1), -- Ram Rider
+(93, 1), -- Electro Dragon
+(94, 1), -- Monk (Champion)
+(95, 1), -- Archer Queen (Champion)
+
+-- 6 Elixir
+(96, 1), -- Royal Giant
+(97, 1), -- Giant Skeleton
+(98, 1), -- Elite Barbarians
+(99, 1), -- Goblin Giant
+(100, 1), -- Sparky
+(101, 1), -- X-Bow
+(102, 1), -- Rocket
+(103, 1), -- Lightning
+
+-- 7 Elixir
+(104, 1), -- P.E.K.K.A
+(105, 1), -- Mega Knight
+(106, 1), -- Lava Hound
+(107, 1), -- Royal Recruits
+(108, 1), -- Barbarian Hut
+
+-- 8 Elixir
+(109, 1), -- Golem
+(110, 1), -- Electro Giant
+
+-- 9 Elixir
+(111, 1), -- Three Musketeers
+
 --Se for assim depois chapa-se as cartas todas, queremos 5000 queries🙏
 
-INSERT INTO Card (item_id, card_name, rarity, card_type, elixir_cost) 
-VALUES (3, 'Knight', 'common', 'ground', 3);
+INSERT INTO Card (item_id, card_name, rarity, card_type, elixir_cost) VALUES
+-- Custo 1
+(3, 'Skeletons', 'common', 'ground', 1),
+(4, 'Ice Spirit', 'common', 'ground', 1),
+(5, 'Fire Spirit', 'common', 'ground', 1),
+(6, 'Electro Spirit', 'common', 'ground', 1),
+(7, 'Heal Spirit', 'rare', 'ground', 1),
+(8, 'Mirror', 'epic', 'spell', 1), -- Custo Variável (+1)
 
-INSERT INTO Card (item_id, card_name, rarity, card_type, elixir_cost) 
-VALUES (4, 'Archers', 'common', 'ground', 3);
+-- Custo 2
+(9, 'Goblins', 'common', 'ground', 2),
+(10, 'Spear Goblins', 'common', 'ground', 2),
+(11, 'Bats', 'common', 'aerial', 2),
+(12, 'Bomber', 'common', 'ground', 2),
+(13, 'Ice Golem', 'rare', 'ground', 2),
+(14, 'Zap', 'common', 'spell', 2),
+(15, 'Giant Snowball', 'common', 'spell', 2),
+(16, 'The Log', 'legendary', 'spell', 2),
+(17, 'Barbarian Barrel', 'epic', 'spell', 2),
+(18, 'Rage', 'epic', 'spell', 2),
+(19, 'Wall Breakers', 'epic', 'ground', 2),
 
-INSERT INTO Card (item_id, card_name, rarity, card_type, elixir_cost) 
-VALUES (5, 'Giant', 'rare', 'ground', 5);
+-- Custo 3
+(20, 'Knight', 'common', 'ground', 3),
+(21, 'Archers', 'common', 'ground', 3),
+(22, 'Minions', 'common', 'aerial', 3),
+(23, 'Goblin Gang', 'common', 'ground', 3),
+(24, 'Mega Minion', 'rare', 'aerial', 3),
+(25, 'Guards', 'epic', 'ground', 3),
+(26, 'Dart Goblin', 'rare', 'ground', 3),
+(27, 'Skeleton Army', 'epic', 'ground', 3),
+(28, 'Tombstone', 'rare', 'building', 3),
+(29, 'Cannon', 'common', 'building', 3),
+(30, 'Goblin Barrel', 'epic', 'spell', 3),
+(31, 'Skeleton Barrel', 'common', 'aerial', 3),
+(32, 'Firecracker', 'common', 'ground', 3),
+(33, 'Arrows', 'common', 'spell', 3),
+(34, 'Clone', 'epic', 'spell', 3),
+(35, 'Tornado', 'epic', 'spell', 3),
+(36, 'Earthquake', 'rare', 'spell', 3),
+(37, 'Royal Delivery', 'common', 'spell', 3),
+(38, 'Void', 'epic', 'spell', 3),
+(39, 'Princess', 'legendary', 'ground', 3),
+(40, 'Ice Wizard', 'legendary', 'ground', 3),
+(41, 'Miner', 'legendary', 'ground', 3),
+(42, 'Bandit', 'legendary', 'ground', 3),
+(43, 'Royal Ghost', 'legendary', 'ground', 3),
+(44, 'Fisherman', 'legendary', 'ground', 3),
+(45, 'Elixir Golem', 'rare', 'ground', 3),
+(46, 'Little Prince', 'champion', 'ground', 3),
 
-INSERT INTO Card (item_id, card_name, rarity, card_type, elixir_cost) 
-VALUES (6, 'Fireball', 'rare', 'spell', 4);
+-- Custo 4
+(47, 'Valkyrie', 'rare', 'ground', 4),
+(48, 'Musketeer', 'rare', 'ground', 4),
+(49, 'Mini P.E.K.K.A', 'rare', 'ground', 4),
+(50, 'Hog Rider', 'rare', 'ground', 4),
+(51, 'Battle Ram', 'rare', 'ground', 4),
+(52, 'Flying Machine', 'rare', 'aerial', 4),
+(53, 'Zappies', 'rare', 'ground', 4),
+(54, 'Furnace', 'rare', 'building', 4),
+(55, 'Bomb Tower', 'rare', 'building', 4),
+(56, 'Tesla', 'common', 'building', 4),
+(57, 'Goblin Cage', 'rare', 'building', 4),
+(58, 'Goblin Drill', 'epic', 'building', 4),
+(59, 'Mortar', 'common', 'building', 4),
+(60, 'Skeleton Dragons', 'common', 'aerial', 4),
+(61, 'Battle Healer', 'rare', 'ground', 4),
+(62, 'Fireball', 'rare', 'spell', 4),
+(63, 'Poison', 'epic', 'spell', 4),
+(64, 'Freeze', 'epic', 'spell', 4),
+(65, 'Electro Wizard', 'legendary', 'ground', 4),
+(66, 'Inferno Dragon', 'legendary', 'aerial', 4),
+(67, 'Night Witch', 'legendary', 'ground', 4),
+(68, 'Lumberjack', 'legendary', 'ground', 4),
+(69, 'Hunter', 'epic', 'ground', 4),
+(70, 'Magic Archer', 'legendary', 'ground', 4),
+(71, 'Mother Witch', 'legendary', 'ground', 4),
+(72, 'Phoenix', 'legendary', 'aerial', 4),
+(73, 'Golden Knight', 'champion', 'ground', 4),
+(74, 'Skeleton King', 'champion', 'ground', 4),
+(75, 'Mighty Miner', 'champion', 'ground', 4),
 
+-- Custo 5
+(76, 'Giant', 'rare', 'ground', 5),
+(77, 'Wizard', 'rare', 'ground', 5),
+(78, 'Witch', 'epic', 'ground', 5),
+(79, 'Prince', 'epic', 'ground', 5),
+(80, 'Baby Dragon', 'epic', 'aerial', 5),
+(81, 'Barbarians', 'common', 'ground', 5),
+(82, 'Minion Horde', 'common', 'aerial', 5),
+(83, 'Rascals', 'common', 'ground', 5),
+(84, 'Royal Hogs', 'rare', 'ground', 5),
+(85, 'Balloon', 'epic', 'aerial', 5),
+(86, 'Cannon Cart', 'epic', 'ground', 5),
+(87, 'Inferno Tower', 'rare', 'building', 5),
+(88, 'Goblin Hut', 'rare', 'building', 5),
+(89, 'Executioner', 'epic', 'ground', 5),
+(90, 'Bowler', 'epic', 'ground', 5),
+(91, 'Graveyard', 'legendary', 'spell', 5),
+(92, 'Ram Rider', 'legendary', 'ground', 5),
+(93, 'Electro Dragon', 'epic', 'aerial', 5),
+(94, 'Monk', 'champion', 'ground', 5),
+(95, 'Archer Queen', 'champion', 'ground', 5),
+
+-- Custo 6
+(96, 'Royal Giant', 'common', 'ground', 6),
+(97, 'Giant Skeleton', 'epic', 'ground', 6),
+(98, 'Elite Barbarians', 'common', 'ground', 6),
+(99, 'Goblin Giant', 'epic', 'ground', 6),
+(100, 'Sparky', 'legendary', 'ground', 6),
+(101, 'X-Bow', 'epic', 'building', 6),
+(102, 'Rocket', 'rare', 'spell', 6),
+(103, 'Lightning', 'epic', 'spell', 6),
+
+-- Custo 7
+(104, 'P.E.K.K.A', 'epic', 'ground', 7),
+(105, 'Mega Knight', 'legendary', 'ground', 7),
+(106, 'Lava Hound', 'legendary', 'aerial', 7),
+(107, 'Royal Recruits', 'common', 'ground', 7),
+(108, 'Barbarian Hut', 'rare', 'building', 7),
+
+-- Custo 8
+(109, 'Golem', 'epic', 'ground', 8),
+(110, 'Electro Giant', 'epic', 'ground', 8),
+
+-- Custo 9
+(111, 'Three Musketeers', 'rare', 'ground', 9),
 
 
 -- Price (Preços na Loja)
